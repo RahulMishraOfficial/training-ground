c2fdd5456c31a334fe8f898ee68a915cedafe05c
RahulMishraOfficial <rahumishraofficial@gmail.com>
2024-05-06 15:49:24 +0530

    06-May-24

--- my.js
+++ my.js
@@ -1,42 +1,48 @@
-const userName = prompt("Enter your user name") || "Procodder"
-const userAge = parseInt(prompt("Enter your age")) || 22 //we can use + for convert string number to nubmer 
+// const userName = prompt("Enter your user name") || "Procodder"
+// const userAge = parseInt(prompt("Enter your age")) || 22 //we can use + for convert string number to nubmer 
   //in parseInt it convert null value to NaN and + convert null value to 0
-const userGender = confirm("Are you male") //if value is true user is male else if value is false user is female
-const userGenderForMale = "he"
-const userGenderForFemale = "she"
-
-console.log(`Name: ${userName}`);
-console.log(`Age: ${userAge}`);
+// const userGender = confirm("Are you male") //if value is true user is male else if value is false user is female
+// const userGenderForMale = "he"
+// const userGenderForFemale = "she"
 
 
-if(userAge > 0 && userAge <= 5 && userGender == true){
-    console.log(`${userName} is kid`);
-    console.log(`and ${userGenderForMale} is playing`);
-}else if(userAge > 0 && userAge <= 5 && userGender == false){
-    console.log(`${userName} is kid`);
-    console.log(`and ${userGenderForFemale} is playing`);
-}else if(userAge > 5 && userAge <= 17 && userGender == true){
-    console.log(`${userName} is a school student`);
-    console.log(`and ${userGenderForMale} is learning science and math`);
-}else if(userAge > 5 && userAge <= 17 && userGender == false){
-    console.log(`${userName} is a school student`);
-    console.log(`and ${userGenderForFemale} is learning science and math`);
-}else if(userAge > 17 && userAge <= 25 && userGender == true){
-    console.log(`${userName} is a collage student`);
-    console.log(`and ${userGenderForMale} is learning compute science`);
-}else if(userAge > 17 && userAge <= 25 && userGender == false){
-    console.log(`${userName} is a collage student`);
-    console.log(`and ${userGenderForFemale} is learning compute science`);
-}else if(userAge > 25 && userAge <= 45 && userGender == true){
-    console.log(`${userName} is working professional`);
-    console.log(`and ${userGenderForMale} is a software developer`);
-}else if(userAge > 25 && userAge <= 45 && userGender == false){
-    console.log(`${userName} is working professional`);
-    console.log(`and ${userGenderForFemale} is a software developer`);
-}else if(userAge > 45 && userAge <= 65 && userGender == true){
-    console.log(`${userName} is retiared`);
-    console.log(`and ${userGenderForMale} reads newspaper`);
-}else if(userAge > 45 && userAge <= 65 && userGender == false){
-    console.log(`${userName} is retiared`);
-    console.log(`and ${userGenderForFemale} reads newspaper`);
-}
+
+// console.log(`Name: ${userName}`);
+// console.log(`Age: ${userAge}`);
+
+if(13) console.log("hello")   //Truthy
+if(!13) console.log("world")  //Falsy
+
+// if(userAge > 0 && userAge <= 5 && userGender == true){
+//     console.log(`${userName} is kid`);
+//     console.log(`and ${userGenderForMale} is playing`);
+// }else if(userAge > 0 && userAge <= 5 && userGender == false){
+//     console.log(`${userName} is kid`);
+//     console.log(`and ${userGenderForFemale} is playing`);
+// }else if(userAge > 5 && userAge <= 17 && userGender == true){
+//     console.log(`${userName} is a school student`);
+//     console.log(`and ${userGenderForMale} is learning science and math`);
+// }else if(userAge > 5 && userAge <= 17 && userGender == false){
+//     console.log(`${userName} is a school student`);
+//     console.log(`and ${userGenderForFemale} is learning science and math`);
+// }else if(userAge > 17 && userAge <= 25 && userGender == true){
+//     console.log(`${userName} is a collage student`);
+//     console.log(`and ${userGenderForMale} is learning compute science`);
+// }else if(userAge > 17 && userAge <= 25 && userGender == false){
+//     console.log(`${userName} is a collage student`);
+//     console.log(`and ${userGenderForFemale} is learning compute science`);
+// }else if(userAge > 25 && userAge <= 45 && userGender == true){
+//     console.log(`${userName} is working professional`);
+//     console.log(`and ${userGenderForMale} is a software developer`);
+// }else if(userAge > 25 && userAge <= 45 && userGender == false){
+//     console.log(`${userName} is working professional`);
+//     console.log(`and ${userGenderForFemale} is a software developer`);
+// }else if(userAge > 45 && userAge <= 65 && userGender == true){
+//     console.log(`${userName} is retiared`);
+//     console.log(`and ${userGenderForMale} reads newspaper`);
+// }else if(userAge > 45 && userAge <= 65 && userGender == false){
+//     console.log(`${userName} is retiared`);
+//     console.log(`and ${userGenderForFemale} reads newspaper`);
+// }
+
+console.log("code ended");
